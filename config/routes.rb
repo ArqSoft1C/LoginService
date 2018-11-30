4eadff35258ed1d16930fd39e95f675f87595033
smarquezo <smarquezo@unal.edu.co>
2018-11-30 01:10:27 -0500

    ldap

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/auth/allUsers', to: 'user#allUsers'
   get '/auth/userById/:id', to: 'user#findUserById'
+  resources :ldap
 end
